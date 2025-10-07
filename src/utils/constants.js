e09c34b2b53817dfc519be2437fa1d9da8b02e21
Tássio Medeiros <tassiohfmed@gmail.com>
2025-10-07 12:07:57 -0300

    feat: constantes para acesso à API

---
+++ src/utils/constants.js
@@ -0,0 +1,25 @@
+export const API_CONFIG = {
+  BASE_URL: 'https://apiteste.mobieduca.me',
+  ENDPOINTS: {
+    LOGIN: '/api/login/run',
+    SCHOOLS: '/api/escolas',
+    CITIES: '/api/cidades',
+    STATES: '/api/estados'
+  },
+  CREDENTIALS: {
+    EMAIL: 'tassiohfmed@gmail.com',
+    PASSWORD: 'tassio@159753'
+  }
+};
+
+export const LOCALIZATION = {
+  1: 'Urbana',
+  2: 'Rural'
+};
+
+export const SHIFTS = {
+  M: 'Manhã',
+  T: 'Tarde', 
+  N: 'Noite',
+  I: 'Integral'
+};
