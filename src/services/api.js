0fd6a9603840da7d8944daa4fb205911b0d9e84a
Tássio Medeiros <tassiohfmed@gmail.com>
2025-10-07 12:04:14 -0300

    feat: implementa cliente HTTP com interceptors para autenticação

---
+++ src/services/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_CONFIG } from "../utils/constants";
+
+const api = axios.create({
+  baseURL: API_CONFIG.BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error('Error getting token:', error);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
